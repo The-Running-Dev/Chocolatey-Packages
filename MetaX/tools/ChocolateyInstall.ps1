50ebb70e65e108dfc69d7788a7e0d1f296a254ef
Boyan Kostadinov <boyank@gmail.com>
2018-05-16 01:26:17 -0500

    Updated to the latest version
    Fixed update script

--- MetaX/tools/ChocolateyInstall.ps1
+++ MetaX/tools/ChocolateyInstall.ps1
@@ -4,9 +4,9 @@ $arguments = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = 'MetaX*'
     url            = 'http://www.danhinsley.com/downloads/MetaXSetup.msi'
-    checksum       = 'AA0CB5CB4E06BD31A0227C0236FBB757E0CBB564B4B8649D7A6A9BEE55549D95'
+    checksum       = ''
     fileType       = 'msi'
-    checksumType   = 'sha256'
+    checksumType   = ''
     silentArgs     = '/quiet'
     validExitCodes = @(0, 1641, 3010)
 }
--- MetaX/update.ps1
+++ MetaX/update.ps1
@@ -1,22 +1,24 @@
-param([switch] $force, [switch] $push)
-
-$packageDir = $PSScriptRoot
-
-. (Join-Path $PSScriptRoot '..\Scripts\update.begin.ps1')
+Import-Module AU
 
 function global:au_GetLatest {
     $releaseUrl = 'http://danhinsley.com/metax/metax.html'
-    $versionRegEx = 'DOWNLOAD&nbsp;MetaX&nbsp;V([0-9\.]+)'
+    $versionRegEx = 'V([0-9\.]+)\sReleased'
     $url = 'http://www.danhinsley.com/downloads/MetaXSetup.msi'
 
     $releasePage = Invoke-WebRequest -Uri $releaseUrl -UseBasicParsing
     $version = ([regex]::match($releasePage.Content, $versionRegEx).Groups[1].Value)
 
-    if ($force) {
-        $global:au_Version = $version
-    }
-
     return @{ Version = $version; Url32 = $url }
 }
 
-. (Join-Path $PSScriptRoot '..\Scripts\update.end.ps1')
+function global:au_SearchReplace {
+    return @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+    }
+}
+
+update -ChecksumFor none
